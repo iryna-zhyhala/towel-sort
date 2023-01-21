5d5bc56acbdbdbeef6757572a25455a05e741049
iryna-zhyhala <iryna.zhyhala@gmail.com>
2023-01-21 23:29:12 +0300

    fead: add decision task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  var resultArrey = [];
+  if (matrix == null) {
+    return [];
+  }
+  var counter = 0;
+  for (var y = 0; y < matrix.length; y++) {
+    for (var x = 0; x < matrix[y].length; x++) {
+      if (y % 2 == 0) {
+        resultArrey[counter] = matrix[y][x];   
+      } else {
+        resultArrey[counter] = matrix[y][matrix[y].length - 1 - x];
+      } 
+      counter++;
+    }
+  }
+  return resultArrey;
 }
